f2f12f5297f69f742b998f54230a3946851f85e7
Hampus Dunström <hampusdunstrom@gmail.com>
2017-04-04 11:35:30 +0200

    working on parsing arguments

--- include/Cli.h
+++ include/Cli.h
@@ -2,15 +2,21 @@
 // Created by hampus on 2017-04-04.
 //
 
+#include <string>
+
 #ifndef INC_3DCOPY_CLI_H
 #define INC_3DCOPY_CLI_H
 
 
 class Cli {
     public:
-        int main();
+        int main(int argc, char* argv[]);
 
     private:
+        std::string source;
+        std::string output_filename;
+        bool source_is_dir;
+        int parse_arguments(int argc, char* argv[]);
 };
 
 
--- src/Cli.cpp
+++ src/Cli.cpp
@@ -3,7 +3,21 @@
 //
 
 #include "../include/Cli.h"
+#include <iostream>
 
-int Cli::main() {
+int Cli::main(int argc, char **argv) {
+    if (parse_arguments(argc, argv)) {
+        std::cout << "Usage: " << argv[0] << " [options] sources output_filename" << std::endl;
+        return 0;
+    }
+
     return 0;
+}
+
+int Cli::parse_arguments(int argc, char **argv) {
+
+    if (argc < 3) {
+        return 1;
+    }
+
 }
