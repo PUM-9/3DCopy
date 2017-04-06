bd7a9c12f302e665b98be84afb3a15a507d3b26a
Hampus Dunström <hampusdunstrom@gmail.com>
2017-04-06 11:04:09 +0200

    added some more tests for testing reading from directories.

--- tests/cli_tests.py
+++ tests/cli_tests.py
@@ -3,6 +3,9 @@ import subprocess
 test_inputs = [
     ["./3DCopy", "one.pcd", "two.pcd", "three.pcd", "output.pcd"],
     ["./3DCopy", "one.pcd", "three.pcd", "output"],
+    ["./3DCopy", "-d", "pcd_files", "output"],
+    ["./3DCopy", "-d", "asg", "output"],
+    ["./3DCopy", "-d", "no_pcd_files", "output"],
     ["./3DCopy", "one.txt", "two.pcd", "three.pcd", "output"],
     ["./3DCopy", "one.pcd", "output.txt"]
     ]
